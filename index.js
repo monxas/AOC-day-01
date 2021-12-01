838a8ae3673dff32eb3b55582f77303a21c37308
Ramon Kamibayashi Carrera <rcarrera@cartrawler.com>
2021-12-01 15:17:53 +0100

    done!

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const fetch = require('node-fetch');	//npm install node-fetch
 
 // fetch the data from the API
 async function getData() {
-const response = await fetch('httpdata.txt');
-const body = await response.text();
+    const response = await fetch('https://raw.githubusercontent.com/monxas/AOC-day-01/develop/data.txt');
+    const body = await response.text();
+    // array of lines linebreak
+    const lines = body.split('\n');
 
-console.log(body);
+    increased = 0;
+    decreased = 0;
+    lastValue = +lines[0];
+    lines.forEach(val => {
+        +val > lastValue ? increased++ : decreased++;
+        lastValue = +val;
+    });
+    console.log(`increased: ${increased}`);
+    console.log(`decreased: ${decreased}`);
 };
 
 getData();
