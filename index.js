046a30c79e302a8b2672f43a1d770ce3afe7b782
Ramon Kamibayashi Carrera <rcarrera@cartrawler.com>
2021-12-02 11:41:24 +0100

    second puzzle done

--- index.js
+++ index.js
@@ -7,15 +7,58 @@ async function getData() {
     // array of lines linebreak
     const lines = body.split('\n');
 
-    increased = 0;
-    decreased = 0;
-    lastValue = +lines[0];
+    firstPart(lines);
+    secondPart(lines);
+};
+
+
+// get price of bitcoin on coingecko api in usd
+async function getPrice() {
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin');
+    const body = await response.json();
+    console.log(body.market_data.current_price.usd);
+};
+
+// function to know if number is even
+function isEven(num) {
+    return num % 2 == 0;
+}
+
+
+
+
+
+
+
+
+
+
+function firstPart(lines) {
+    let increased = 0;
+    let decreased = 0;
+    let lastValue = +lines[0];
     lines.forEach(val => {
         +val > lastValue ? increased++ : decreased++;
         lastValue = +val;
     });
-    console.log(`increased: ${increased}`);
-    console.log(`decreased: ${decreased}`);
-};
+    console.log(`increased: ${increased} 🎉 `); 
+    // console.log(`decreased: ${decreased}`);
+}
 
+function secondPart(lines) {
+    let increased = 0;
+    let decreased = 0;
+    let lastValue = +lines[0]+lines[1]+lines[2];
+    const lastIndex = lines.length - 1;
+
+    lines.forEach((val, i) => {
+        if (i+2 <= lastIndex) {
+            let currentValue = +lines[i] + +lines[i+1] + +lines[i+2];
+            currentValue > lastValue ? increased++ : decreased++;
+            lastValue = currentValue;
+        }
+    });
+    console.log(`increased: ${increased} 🎉 `); 
+    // console.log(`decreased: ${decreased}`);
+}
 getData();
